fc041e0af3b25d8947cf373869a05c66e70323b8
David-Belizario <david.belizario@tecsup.edu.pe>
2024-11-28 13:19:22 -0500

    Navegacion basica de LoginScreen a HomeScreen

---
+++ app/src/main/java/com/example/fitnessapp/ui/screens/HomeScreens/HomeScreen.kt
@@ -0,0 +1,14 @@
+package com.example.fitnessapp.ui.screens.HomeScreens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+    Column {
+        Text(text = "Ventana de inicio de la aplicación")
+    }
+
+}
